91f88ea5503d7547cca352cfa84dc877aa1adbc9
Ravi Kanth <anjan.305@gmail.com>
2025-02-05 00:36:53 -0500

    Implement EpisodesListView and EpisodeRow to display episodes

--- APMC/APMC/Views/Components/EpisodeRow.swift
+++ APMC/APMC/Views/Components/EpisodeRow.swift
@@ -5,4 +5,33 @@
 //  Created by Ravi Kanth Bollam on 2025-02-03.
 //
 
-import Foundation
+import SwiftUI
+
+struct EpisodeRow: View {
+    let episode: Episode
+
+    var body: some View {
+        HStack(spacing: 16) {
+            // Placeholder image for episode thumbnail
+            Image(systemName: "film")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.gray)
+                .padding(8)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+
+            VStack(alignment: .leading, spacing: 8) {
+                Text(episode.title)
+                    .font(.headline)
+                Text("Duration: \(episode.duration.formattedTime())")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding(.vertical, 8)
+    }
+}
+
+
+
