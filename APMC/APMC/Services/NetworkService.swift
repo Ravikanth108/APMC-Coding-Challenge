733cd25d89f4b98ea2c92e11d1c5a85c86f2b8fc
Ravi Kanth <anjan.305@gmail.com>
2025-02-05 00:34:03 -0500

    Implement NetworkService to fetch episodes from API

--- APMC/APMC/Services/NetworkService.swift
+++ APMC/APMC/Services/NetworkService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol NetworkServiceProtocol {
+    func fetchEpisodes() async throws -> [Episode]
+}
+
+class NetworkService: NetworkServiceProtocol {
+    private let urlString = "https://run.mocky.io/v3/8419df8e-0160-4a35-a1e6-0237a527c566"
+    
+    func fetchEpisodes() async throws -> [Episode] {
+        guard let url = URL(string: urlString) else { throw URLError(.badURL) }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Episode].self, from: data)
+    }
+}
